3b79eb10a7c7d8f5c4f2f453e2f2f0263fa90e3a
M-Kgobe <kgobemalesela@gmail.com>
2022-07-06 14:36:45 +0200

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+function Header(){
+    return(
+        <header>
+            <div>
+                your header goes here
+            </div>
+            <nav>
+                the nav goes here
+                <li><a href="#!" alt="none">sd</a></li>
+                <li><a href="#!" alt="none">sd</a></li>
+                <li><a href="#!" alt="none">sd</a></li>
+                <li><a href="#!" alt="none">sd</a></li>
+                <li><a href="#!" alt="none">sd</a></li>
+            </nav>
+
+        </header>
+    )
+}
+
+export default Header
