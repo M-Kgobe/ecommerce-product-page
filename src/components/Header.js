da0d3c109bca6067d97f9bc45a8acf10047c1973
M-Kgobe <kgobemalesela@gmail.com>
2022-07-06 16:38:10 +0200

    initial nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,26 @@
 import React from "react"
+import logo from "../images/logo.svg"
+import avatar from "../images/image-avatar.png"
+import cart from "../images/icon-cart.svg"
 
 function Header(){
     return(
-        <header>
-            <div>
-                your header goes here
+        <header id="header">
+            <div className="logo">
+                <img src={logo} alt="logo"/>
             </div>
-            <nav>
-                the nav goes here
-                <li><a href="#!" alt="none">sd</a></li>
-                <li><a href="#!" alt="none">sd</a></li>
-                <li><a href="#!" alt="none">sd</a></li>
-                <li><a href="#!" alt="none">sd</a></li>
-                <li><a href="#!" alt="none">sd</a></li>
+            <nav className="nav-bar">
+                <ul className="nav-links">
+                    <li><a href="#collections">Collections</a></li>
+                    <li><a href="#men">Men</a></li>
+                    <li><a href="#women">Women</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+                <ul className="account">
+                    <li><a href="#!"><img src={cart} alt="cart"/></a></li>
+                    <li><a href="#!"><img src={avatar} alt="avatar"/></a></li>
+                </ul>
             </nav>
 
         </header>
